d9906e7ddd906a6b3e269799f1ca1034313eebed
unknown <pedromartinez_goyo@hotmail.com>
2020-09-10 18:17:09 -0500

    First commit + basics, some operators

---
+++ primer/basics.js
@@ -0,0 +1,42 @@
+const c = console.log;
+c("Bienvenido a RXJS!!");
+
+//const {fromEvent} = require('rxjs');
+
+// Creando Observable
+const btnTest1 = document.getElementById('btnTest1');
+const myObservable = rxjs.fromEvent(btnTest1, 'click');
+
+// Subscribiendo el Observer con una function
+const mySubscription = myObservable.subscribe(event => {
+    c('subscription to event:', event);
+   
+});
+
+// Subscribiendo con un Object
+const mySubscription2 = myObservable.subscribe({
+    next: event => {
+        c('second subscription', event);
+       
+    },
+    error: err => c('something went wrong', err),
+    complete: () => c("Everything is working clean!!")
+});
+
+mySubscription2.unsubscribe(() => {});
+
+// Creamos un Observable a partir de argumentos
+const numbers = rxjs.of(1,2,3,4,5,6);
+// Creamos un pipe de operators y subscribimos
+const numbersSubscription = numbers.pipe(
+    rxjs.operators.filter(num => num >= 4),
+    rxjs.operators.map(num => {
+        throw new Error("Error thrown");
+        return ++num;
+    }),
+    rxjs.operators.catchError(err => c("Caught Error!!!"))
+).subscribe((num) => {
+    c(num);
+    
+});
+
