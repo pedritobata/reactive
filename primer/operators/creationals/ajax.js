af96468422b60402899ae0ab1c16d2b00ae7b15f
pedro martinez <pedromartinez_goyo@hotmail.com>
2020-10-06 08:14:34 -0500

    ajax methods finished

--- primer/operators/creationals/ajax.js
+++ primer/operators/creationals/ajax.js
@@ -7,3 +7,39 @@ const obs$ = rxjs.ajax.ajax('https://jsonplaceholder.typicode.com/users').pipe(
     rxjs.operators.catchError(err => console.log(err))
 )
 obs$.subscribe(user => console.log(user.name," - ", user.email));
+
+//The same but using getJSON()
+
+const obs2$ = rxjs.ajax.ajax.getJSON('https://jsonplaceholder.typicode.com/users').pipe(
+    rxjs.operators.flatMap(response => {
+        return rxjs.from(response);
+    }),
+    rxjs.operators.catchError(err => console.log(err))
+).subscribe(user => console.log(user.username));
+
+const obs3$ = rxjs.ajax.ajax({
+    url: "https://jsonplaceholder.typicode.com/users",
+    method: 'POST',
+    body: JSON.stringify({
+        name: "pedro",
+        username: "pedritobata",
+        email: "pedritobata@gmail.com"
+    }),
+    headers: {
+        'Content-Type': "application/json; charset=UTF-8"
+    }
+
+}).pipe(
+    rxjs.operators.map(response => {
+         return response;
+    }),
+    rxjs.operators.catchError(err => console.log(err))
+).subscribe(resp => console.log(resp.status, resp.response));
+
+// retornando el error
+const obs4$ = rxjs.ajax.ajax("https://jsonplaceholder.typicode.com/userss").pipe(
+    rxjs.operators.map(response => rxjs.of(response)),
+    rxjs.operators.catchError(err => {
+        return rxjs.of(err);
+    })
+).subscribe(resp => console.log(resp));
